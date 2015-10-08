696e8a882e531c390608ccb53bd276924535a0be
Robert Clark <rhclark.pitt@gmail.com>
2015-10-08 12:15:33 -0400

    wsx

--- test_git_utils.sh
+++ test_git_utils.sh
@@ -4,13 +4,13 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
     echo $TRAVIS_OS_NAME
     uname -a
     
-    install python-pip
-    install python-dev
-    install python-numpy
-    install libxml2-dev
-    install zlib1g-dev
-    install libbz2-dev
-    pip install --user python-libsbml
+#    install python-pip
+#    install python-dev
+#    install python-numpy
+#    install libxml2-dev
+#    install zlib1g-dev
+#    install libbz2-dev
+#    pip install --user python-libsbml
 else
     echo "This should say osx"
     echo $TRAVIS_OS_NAME
@@ -24,4 +24,5 @@ else
     brew install libsbml
     brew link --overwrite python
     /usr/local/bin/pip install --user python-libsbml
+    cd ./bng2/Network3 && mkdir -p m4 && cd .. && perl make_dist.pl --build --validate
 fi
